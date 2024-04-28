6d41acb98ca116873d8ecfd459e999e699404b4e
nusrat3657 <nusratjahan3657r@gmail.com>
2024-04-29 03:37:34 +0600

    get oneFind id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,19 @@ async function run() {
     await client.connect();
 
     const spotCollection = client.db('spotDB').collection('spot');
+
+    app.get('/spot', async(req, res) => {
+        const cursor = spotCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/spot/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await spotCollection.findOne(query);
+        res.send(result);
+    })
 
     app.post('/spot', async(req, res) => {
         const newSpot = req.body;
