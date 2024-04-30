21f5c631fbea564ba02f6e8b110d4aecfbdd74a7
nusrat3657 <nusratjahan3657r@gmail.com>
2024-04-30 18:24:07 +0600

    added a link

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: ["http://localhost:5173", "https://assignment-10-5e6b0.web.app"],
+    credentials: true,
+  };
+  app.use(cors(corsConfig));
 app.use(express.json());
 
 
@@ -88,7 +93,7 @@ async function run() {
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
